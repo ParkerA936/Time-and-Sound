95fcd9e166b8313fb0c5bbb1dd2064b3d99e0141
Parker Arnett <362581936@GoTVDSB.ca>
2026-04-27 13:17:41 -0400

    Mostly Donee

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,13 +10,19 @@ namespace Time_and_Sound
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Texture2D bombTexture;
-        Rectangle window, rectBomb;
+        MouseState mouseState;
+        Texture2D bombTexture, pliersTexture;
+        Rectangle window, rectBomb, resetRect, pliersRect;
         SpriteFont TimeFont;
+        SoundEffect explosion;
+        float seconds;
+        bool exploded;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            this.Window.Title = "Lesson 4 - Sound and Time";
             IsMouseVisible = true;
         }
 
@@ -28,8 +35,10 @@ namespace Time_and_Sound
             _graphics.ApplyChanges();
 
             rectBomb = new Rectangle(50, 50, 700, 400);
-
-
+            resetRect = new Rectangle(253, 132,10 ,16 );
+            seconds = 0f;
+            exploded = false;
+            pliersRect = new Rectangle(0, 0, 0,0);
             base.Initialize();
         }
 
@@ -39,14 +48,28 @@ namespace Time_and_Sound
 
             // TODO: use this.Content to load your game content here
             bombTexture = Content.Load<Texture2D>("bomb");
+            TimeFont = Content.Load<SpriteFont>("TimeFont");
+            explosion = Content.Load<SoundEffect>("explosion");
+            pliersTexture = Content.Load<Texture2D>("pliers");
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            mouseState = Mouse.GetState();
+            this.Window.Title = mouseState.Position.ToString();
+            if (mouseState.LeftButton == ButtonState.Pressed && resetRect.Contains(mouseState.Position))
+                seconds = 0f;
             // TODO: Add your update logic here
+            if (!exploded)
+            seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (seconds > 10)
+            {
+                exploded = true;
+                seconds = 0;
+                explosion.Play();
+            }
 
             base.Update(gameTime);
         }
@@ -55,6 +78,13 @@ namespace Time_and_Sound
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(bombTexture, rectBomb, Color.White);
+             _spriteBatch.DrawString(TimeFont, seconds.ToString("00.0"), new Vector2(270,200 ), Color.Black); 
+
+
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
